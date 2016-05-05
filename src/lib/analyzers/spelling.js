8251de9735ea133c2cfbc02c49be7292df09e722
Ken Dale <ken@kendaleiv.com>
2016-05-05 08:57:16 -0400

    Fix spelling not using configuration file

--- src/lib/analyzers/spelling.js
+++ src/lib/analyzers/spelling.js
@@ -1,6 +1,8 @@
 import AnalyzerResult from '../analyzer-result';
 import Location from '../location';
 import markdownSpellcheck from 'markdown-spellcheck';
+import spellConfig from 'markdown-spellcheck/es5/spell-config';
+import spellcheck from 'markdown-spellcheck/es5/spellcheck';
 
 const opts = {
   ignoreAcronyms: true,
@@ -12,19 +14,33 @@ const opts = {
 };
 
 export default class SpellingAnalyzer {
+  constructor(args = { configurationFile: './.spelling' }) {
+    this.configurationFile = args.configurationFile;
+  }
+
   analyze(str) {
-    const result = new AnalyzerResult();
+    return new Promise(resolve => {
+      spellConfig.initialise(this.configurationFile, () => {
+        const result = new AnalyzerResult();
 
-    const spellResult = markdownSpellcheck.spell(str, opts);
+        spellcheck.initialise(opts);
 
-    spellResult.forEach(x => {
-      result.addMessage(
-        'spelling-error',
-        x.word,
-        Location.getLine(str, x.index),
-        Location.getLineColumn(str, x.index));
+        spellConfig
+          .getGlobalWords()
+          .forEach(x => spellcheck.addWord(x, /* temporary: */ true));
+
+        const spellResult = markdownSpellcheck.spell(str, opts);
+
+        spellResult.forEach(x => {
+          result.addMessage(
+            'spelling-error',
+            x.word,
+            Location.getLine(str, x.index),
+            Location.getLineColumn(str, x.index));
+        });
+
+        resolve(result);
+      });
     });
-
-    return result;
   }
 }
--- test/analyzers/spelling.js
+++ test/analyzers/spelling.js
@@ -5,19 +5,23 @@ import SpellingAnalyzer from '../../src/lib/analyzers/spelling';
 test('Returns spelling-error for one spelling error', t => {
   const testText = 'abc123';
 
-  const result = new SpellingAnalyzer().analyze(testText);
-
-  t.is(result.messages.length, 1);
-  t.is(result.getMessage('spelling-error').text, 'abc123');
+  new SpellingAnalyzer()
+    .analyze(testText)
+    .then(result => {
+      t.is(result.messages.length, 1);
+      t.is(result.getMessage('spelling-error').text, 'abc123');
+    });
 });
 
 test('Returns two spelling-error messages for two spelling errors', t => {
   const testText = 'abc123 abc123';
 
-  const result = new SpellingAnalyzer().analyze(testText);
-
-  t.is(result.messages.length, 2);
-  t.true(result.messages.every(x => x.text === 'abc123'));
+  return new SpellingAnalyzer()
+    .analyze(testText)
+    .then(result => {
+      t.is(result.messages.length, 2);
+      t.true(result.messages.every(x => x.text === 'abc123'));
+    });
 });
 
 test('Should return expected line number', t => {
@@ -30,9 +34,11 @@ is
 a
 test.`;
 
-  const result = new SpellingAnalyzer().analyze(testText);
-
-  t.is(result.getMessage('spelling-error').line, 4);
+  return new SpellingAnalyzer()
+    .analyze(testText)
+    .then(result => {
+      t.is(result.getMessage('spelling-error').line, 4);
+    });
 });
 
 test('Should return expected column', t => {
@@ -45,7 +51,19 @@ is
 a
 test.`;
 
-  const result = new SpellingAnalyzer().analyze(testText);
+  return new SpellingAnalyzer()
+    .analyze(testText)
+    .then(result => {
+      t.is(result.getMessage('spelling-error').column, 16);
+    });
+});
 
-  t.is(result.getMessage('spelling-error').column, 16);
+test('Should use spelling configuration file', t => {
+  const testText = 'allowedtestword';
+
+  return new SpellingAnalyzer({ configurationFile: '../../.spelling' })
+    .analyze(testText)
+    .then(result => {
+      t.is(result.messages.length, 0);
+    });
 });
